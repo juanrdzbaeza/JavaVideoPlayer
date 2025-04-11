95565904d99b4e529eddf52cf50ce642d7e53ac9
Juan <juan.rodriguez@edosoft.es>
2025-04-11 10:25:14 +0100

    Adds UI elements for video control
    Adds UI elements, including buttons, sliders, and a container,
    for controlling video playback. Also, introduces fields for
    managing media, player state (looping, mute), and file information.

--- src/main/java/com/juanrdzbaeza/javavideoplayer/VideoPlayerController.java
+++ src/main/java/com/juanrdzbaeza/javavideoplayer/VideoPlayerController.java
@@ -2,6 +2,8 @@ package com.juanrdzbaeza.javavideoplayer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -10,6 +12,10 @@ import javafx.stage.FileChooser;
 import java.io.File;
 
 public class VideoPlayerController {
+
+    @FXML
+    private StackPane mediaViewContainer;
+
     @FXML
     private MediaView mediaView;
 
@@ -21,6 +27,24 @@ public class VideoPlayerController {
 
     @FXML
     private Button stopButton;
+
+    @FXML
+    private Button repeatButton;
+
+    @FXML
+    private Button muteButton;
+
+    @FXML
+    private Slider mediaSlider;
+
+    @FXML
+    private Slider volumeSlider;
+
+    private File file;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private boolean isLooping = false;
+    private boolean isMute = false;
 
     private MediaPlayer mediaPlayer;
 
