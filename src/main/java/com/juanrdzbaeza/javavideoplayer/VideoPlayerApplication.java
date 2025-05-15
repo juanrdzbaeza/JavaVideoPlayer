70a87b04ca4ad0c88bed9b4583159a81643750e2
Juan <juan.rodriguez@edosoft.es>
2025-05-15 09:16:55 +0100

    Añade icono a la aplicación
    Incorpora un icono a la aplicación para mejorar su identificación visual.
    
    Muestra un mensaje de error si el icono no se encuentra.

--- src/main/java/com/juanrdzbaeza/javavideoplayer/VideoPlayerApplication.java
+++ src/main/java/com/juanrdzbaeza/javavideoplayer/VideoPlayerApplication.java
@@ -3,9 +3,11 @@ package com.juanrdzbaeza.javavideoplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class VideoPlayerApplication extends Application {
     @Override
@@ -13,6 +15,12 @@ public class VideoPlayerApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(VideoPlayerApplication.class.getResource("video-player-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Video Player");
+        // Set the application icon
+        try {
+            stage.getIcons().add(new Image(Objects.requireNonNull(VideoPlayerApplication.class.getResourceAsStream("icon.png"))));
+        } catch (NullPointerException e) {
+            System.err.println("Icon not found: icon.png");
+        }
         stage.setScene(scene);
         stage.show();
     }
